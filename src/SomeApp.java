e2bc89e321b3110055f07f863f153b0f4b28e0d9
gsylverman <gsylverman@gmail.com>
2020-09-22 22:29:06 +0300

    filter files/Folder parameter

--- src/SomeApp.java
+++ src/SomeApp.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 
@@ -12,8 +13,23 @@ public class SomeApp {
     }
 
     private static void readDirectories(Path path) {
-        //second param is for filter directory content ex: "*.dat"
-        try (DirectoryStream<Path> contents = Files.newDirectoryStream(path, "*.txt")) {
+        // Method reference
+        DirectoryStream.Filter<Path> filter = Files::isRegularFile;
+
+        //with Lambda
+//        DirectoryStream.Filter<Path> filter = (Path entry) -> Files.isRegularFile(entry);
+
+        // Regular implemented method
+//        DirectoryStream.Filter<Path> filter =
+//                new DirectoryStream.Filter<Path>() {
+//                    @Override
+//                    public boolean accept(Path entry) throws IOException {
+//                        return (Files.isRegularFile(entry));
+//                    }
+//                };
+
+        //second param is for filter directory content ex: "*.dat" or we can replace with our implementation in accept method from DirectoryStream.Filter<Path> interface
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(path, filter)) {
             for (Path item : contents) {
                 System.out.println(item.getFileName());
             }
