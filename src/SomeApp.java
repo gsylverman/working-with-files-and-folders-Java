6a41d19f1bb94ef8dbf524cf4dd27498f6327f89
gsylverman <gsylverman@gmail.com>
2020-09-22 22:09:02 +0300

    iterate throw the content of directiry

--- src/SomeApp.java
+++ src/SomeApp.java
@@ -1,27 +1,31 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.*;
 
 public class SomeApp {
     public static void main(String[] args) {
 
 //        Path folderThree = FileSystems.getDefault().getPath("folderThree", "Dir1\\Dir2\\Dir4");
 //        Path readDirectories = FileSystems.getDefault().getPath("folderThree\\Dir1");
-//
-//        readDirectories();
+
+        Path directory = FileSystems.getDefault().getPath("folderThree", "Dir1");
+        readDirectories(directory);
     }
 
     private static void readDirectories(Path path) {
-
+        //second param is for filter directory content ex: "*.dat"
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(path, "*.txt")) {
+            for (Path item : contents) {
+                System.out.println(item.getFileName());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private static void createDirectories(Path path) {
         try {
             Files.createDirectories(path);
-        } catch (IOException e) {
+        } catch (IOException | DirectoryIteratorException e) {
             e.printStackTrace();
         }
     }
